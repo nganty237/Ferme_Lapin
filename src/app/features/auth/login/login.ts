0499441e1cebc4ae96160b158f5ec453e3427237
nganty237 <surflinkayos@gmail.com>
2026-07-23 19:44:50 +0100

    feat : implémentation de la page de gestion des paramètres de configuration et de l'interface de connexion

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,6 +1,6 @@
 import { Component, signal, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,7 +12,6 @@ import { AuthService, ToastService } from '@core/services';
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    RouterLink,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
